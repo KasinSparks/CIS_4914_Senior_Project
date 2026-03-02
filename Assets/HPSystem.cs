be933be8a5258287a08c2f35da8d7cb1720e39a8
GabrielZardoya <68284996+GabrielZardoya@users.noreply.github.com>
2026-03-02 10:52:27 -0500

    Basic HP System implementation
    Author:    GabrielZardoya <68284996+GabrielZardoya@users.noreply.github.com>
    Date:      Mon Mar 2 10:52:27 2026 -0500

--- Assets/HPSystem.cs
+++ Assets/HPSystem.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class HPSystem : MonoBehaviour
 {
     public int hp;
-    public bool isdefeated;
+    public bool is_defeated;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         this.hp = 10;
-        this.isdefeated = false;
+        this.is_defeated = false;
     }
 
     // Update is called once per frame
@@ -22,18 +22,17 @@ public class HPSystem : MonoBehaviour
     {
         this.hp -= damage;
 
-        if(this.hp <= 0)
+        if (this.hp <= 0)
         {
             this.hp = 0;
-
-            if(this.isdefeated == false)
-            {
-                this.isdefeated = true;
-                UnityEngine.Debug.Log(this.name + " has been defeated");
-            }
-            
+            this.is_defeated = true;
         }
 
+    }
+
+    public void Heal(int healing)
+    {
+        this.hp += healing;
     }
 
 }
