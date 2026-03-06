78a48d22df797ef2891c5e96c3c83005455d8998
KasinSparks <kasinsparks@gmail.com>
2026-03-06 14:43:33 -0500

    Working save system for the consumables

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -2,27 +2,120 @@ using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public enum SaveSystemFile
 {
     None,
     PlayerDeck,
+    PlayerConsumables,
     OpponentDeck1,  // Example
     OpponentDeck2,  // Example, can change this to the name of the opponent
 }
 
 public class SaveSystem
 {
-    static readonly string SAVES_FOLDER        = "SAVES";
-    static readonly string DECK_FOLDER         = "DECKS";
-    static readonly string DECK_SAVE_LOCATION  = Path.Combine(SAVES_FOLDER, DECK_FOLDER);
+    private static readonly string SAVES_FOLDER = "SAVES";
 
+    private static readonly string DECK_FOLDER         = "DECKS";
+    private static readonly string DECK_SAVE_LOCATION  = Path.Combine(SAVES_FOLDER, DECK_FOLDER);
+
+    private static readonly string CONSUMABLES_FOLDER         = "CONSUMABLES";
+    private static readonly string CONSUMABLES_SAVE_LOCATION  = Path.Combine(SAVES_FOLDER, CONSUMABLES_FOLDER);
+
+    /**
+     * @breif Gets the save file name for the given save file type.
+     * @param The save file type.
+     * @return The actual save file name.
+     */
+    private static string _GetSaveFileName(SaveSystemFile file)
+    {
+        switch (file)
+        {
+            case SaveSystemFile.PlayerDeck:
+                return "PLAYER_DECK.json";
+            case SaveSystemFile.PlayerConsumables:
+                return "PLAYER_CONSUMABLES.json";
+            case SaveSystemFile.OpponentDeck1:
+                return "OPPONENT1_DECK.json";
+
+            default:
+                // TODO(KASIN):
+                throw new System.NotImplementedException();
+        }
+    }
+    
+    /**
+     * @brief Gets the directory path for the file.
+     * @param The save file type.
+     * @return The Directory Path for the save file.
+     */
+    private static string _GetSaveFileLocation(SaveSystemFile file)
+    {
+        switch (file)
+        {
+            case SaveSystemFile.PlayerDeck:
+                return DECK_SAVE_LOCATION;
+            case SaveSystemFile.PlayerConsumables:
+                return CONSUMABLES_SAVE_LOCATION;
+            case SaveSystemFile.OpponentDeck1:
+                // TODO
+                return "";
+
+            default:
+                // TODO(KASIN):
+                throw new System.NotImplementedException();
+        }
+    }
+
+    /**
+     * @brief Gets the directory path including the file name.
+     * @param The save file type.
+     * @return The entire Directory Path including the file.
+     */
+    private static string GetFullPath(SaveSystemFile file)
+    {
+        return Path.Combine(_GetSaveFileLocation(file), _GetSaveFileName(file));
+    }
+
+    
+    /**
+     * @brief Save the JSON string to a file
+     * @param json The JSON string.
+     * @param file The file type
+     * @param mode The writing mode for the file. (Append, create, etc.)
+     */
+    private static void SaveToJsonFile(string json, SaveSystemFile file, FileMode mode = FileMode.Create)
+    {
+        // Check if the save folders exists
+        if (!Directory.Exists(SAVES_FOLDER))
+        {
+            Directory.CreateDirectory(SAVES_FOLDER);
+        }
+
+        // Check if the path save subfolder exists
+        if (!Directory.Exists(_GetSaveFileLocation(file)))
+        {
+            Directory.CreateDirectory(_GetSaveFileLocation(file));
+        }
+        
+        // TODO(KASIN): This may need error handling
+        FileStream fs = new FileStream(GetFullPath(file), mode);
+        StreamWriter output = new StreamWriter(fs);
+        output.Write(json);
+        output.Flush();
+        output.Close();
+
+        if (fs != null)
+        {
+            fs.Close();
+        }
+    }
     /**
      * @brief Save the deck of cards to a file
      * @param cards The cards that compose the deck
      * @param file The deck save file
      */
+
     public static void SaveDeck(CardData[] cards, SaveSystemFile file)
     {
         // TODO(KASIN): For now, each line will represent a different card
@@ -52,7 +145,7 @@ public class SaveSystem
         StreamReader reader = null;
         try
         {
-            reader = new StreamReader(Path.Combine(DECK_SAVE_LOCATION, GetSaveFileName(file)));
+            reader = new StreamReader(GetFullPath(file));
         }
         catch (System.IO.FileNotFoundException)
         {
@@ -130,45 +223,108 @@ public class SaveSystem
     }
     
     /**
-     * @breif Gets the save file name for the given save file type.
-     * @param The save file type.
-     * @return The actual save file name.
+     * @brief Check to see if the consumable file exists.
+     * @param The file type.
+     * @return True if the file already exists. 
      */
-    private static string GetSaveFileName(SaveSystemFile file)
+    public static bool CheckForConsumableFileExistence(SaveSystemFile file)
     {
-        switch (file)
-        {
-            case SaveSystemFile.PlayerDeck:
-                return "PLAYER_DECK.json";
-            case SaveSystemFile.OpponentDeck1:
-                return "OPPONENT1_DECK.json";
-
-            default:
-                // TODO(KASIN):
-                throw new System.NotImplementedException();
-        }
+        return File.Exists(GetFullPath(file));
+    }
+    
+    /**
+     * @brief Remove the file given.
+     * @param file The file to delete.
+     */
+    private static void DeleteSaveFile(SaveSystemFile file)
+    {
+        File.Delete(GetFullPath(file));
     }
 
-    private static void SaveToJsonFile(string json, SaveSystemFile file)
+    /**
+     * @brief Adds the consumable data to the end of the file.
+     * @param consumable The consumable object.
+     * @param file The Consumable SaveSystemFile type.
+     * @param add_newline Set to True if this should this add a newline at the end.
+     */
+    public static void AppendConsumableToSaveFile(ScriptableObject consumable, SaveSystemFile file, bool add_newline = true)
     {
-        string file_name = GetSaveFileName(file);
-
-        // Check if the save folders exists
-        if (!Directory.Exists(SAVES_FOLDER))
+        string json_string = JsonUtility.ToJson(consumable);
+        StringBuilder sb = new StringBuilder();
+        sb.Append(consumable.GetType().ToString());
+        sb.Append(" : ");
+        sb.Append(json_string);
+        if (add_newline)
         {
-            Directory.CreateDirectory(SAVES_FOLDER);
+            sb.Append("\n");
+        }
+        SaveToJsonFile(sb.ToString(), file, FileMode.Append);
+    }
+    
+    /**
+     * @brief Loads all the consumable data from the save file.
+     * @param file The SaveSystemFile type for the consumable.
+     * @return The consumables as a object with type information inside.
+     */
+    public static ScriptableObject[] LoadConsumablesFromSaveFile(SaveSystemFile file)
+    {
+        List<ScriptableObject> consumables = new List<ScriptableObject>();
+
+        StreamReader reader = null;
+        try
+        {
+            reader = new StreamReader(GetFullPath(file));
+        }
+        catch (System.IO.FileNotFoundException)
+        {
+            return null;
         }
 
-        // Check if the path save subfolder exists
-        if (!Directory.Exists(DECK_SAVE_LOCATION))
+        string line = reader.ReadLine();
+        while (line != null)
         {
-            Directory.CreateDirectory(DECK_SAVE_LOCATION);
+            ScriptableObject obj = null;
+            if (line != null && line != "")
+            {
+                string[] parts = line.Split(" : ");
+                // LHS has the original class type information
+                obj = ScriptableObject.CreateInstance(parts[0]);
+                // RHS will be the scriptable object data
+                JsonUtility.FromJsonOverwrite(parts[1], obj);
+                consumables.Add(obj);
+            }
+
+            line = reader.ReadLine();
         }
 
+        reader.Close();
 
-        StreamWriter output = new StreamWriter(Path.Combine(DECK_SAVE_LOCATION, file_name));
-        output.Write(json);
-        output.Flush();
-        output.Close();
+        return consumables.ToArray();
+    }
+    
+    /**
+     * @brief Save the consumables to the save file. Will overwrite existing
+     * data. Will create an empty file if there are no consumables in the
+     * array passed in.
+     * @param consumables The array of consumables that will be saved
+     * @param file The SaveSystemFile type for the consumables.
+     */
+    public static void SaveConsumablesToFile(ScriptableObject[] consumables, SaveSystemFile file)
+    {
+        // Clear existing save file
+        SaveSystem.DeleteSaveFile(file);
+        File.Create(GetFullPath(file)).Close();
+        
+        // Write the new data to the save file
+        bool add_newline = true;
+        for (int i = 0; i < consumables.Length; ++i)
+        {
+            if (i == consumables.Length - 1)
+            {
+                add_newline = false;
+            }
+
+            AppendConsumableToSaveFile(consumables[i], file, add_newline);
+        }
     }
 }
