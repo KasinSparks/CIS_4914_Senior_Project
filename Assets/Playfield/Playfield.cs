c387eb53e56c978445146771deb438129d280d00
austinnickse <goobersimon96@gmail.com>
2026-04-14 00:26:36 -0400

    Update Playfield.cs

--- Assets/Playfield/Playfield.cs
+++ Assets/Playfield/Playfield.cs
@@ -353,7 +353,7 @@ public class Playfield : MonoBehaviour
             }
 
             this.SetSacrificeButtonActive(false);
-            this.game_state.UpdateTurnState(this.GetTurnStatePriorToSacrifice());
+            //this.game_state.UpdateTurnState(this.GetTurnStatePriorToSacrifice()); this line makes it so every other card can be played for free
         }
 
     }
